5c2a90cf97c0793d9994dcfab0184175af531995
Daniel Brito <danielbrito@pq.uenf.br>
2022-02-21 23:56:11 -0300

    take-a-number working!

--- take-a-number/lib/take_a_number.ex
+++ take-a-number/lib/take_a_number.ex
@@ -1,18 +1,23 @@
 defmodule TakeANumber do
-  defp machine(s \\ 0) do
-    state = s
+  defp loop(state) do
     receive do
       {:report_state, sender_pid} ->
         send(sender_pid,state)
+        loop(state)
       {:take_a_number, sender_pid} ->
-        send(sender_pid, state + 1)
-        machine(s + 1)
-      {:stop, _sender_pid} ->
+        state = state + 1
+        send(sender_pid, state)
+        loop(state)
+      :stop ->
         nil
+      _ ->
+        loop(state)
     end
-
+  end
+  defp machine(state) do
+    loop(state)
   end
   def start() do
-    spawn(machine())
+    spawn(fn -> machine(0) end)
   end
 end
