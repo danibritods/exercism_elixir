9e7dfc6224027d9c23e03340ea879b33e42c2eaa
Daniel Brito <danielbrito@pq.uenf.br>
2022-02-21 23:30:21 -0300

    take-a-number first draft

--- take-a-number/lib/take_a_number.ex
+++ take-a-number/lib/take_a_number.ex
@@ -1,5 +1,20 @@
 defmodule TakeANumber do
+  #defp machine(state) when state == 0,do: 0
+  defp machine(init \\ nil) do
+    if init == 0 do
+      s = 0
+    else
+      s = s + 1
+    end
+    receive do
+      {:report_state, sender_pid} ->
+        send(sender_pid,s)
+      {:take_a_number, sender_pid} ->
+        send(sender_pid, machine())
+    end
+    s
+  end
   def start() do
-    spawn()
+    spawn(machine(0))
   end
 end
